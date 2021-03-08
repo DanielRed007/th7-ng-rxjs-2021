01d63ad5b9fdc31d66c4a0870243cfad8fb313d6
DanielRed007 <danielrojo1927@gmail.com>
2021-03-08 16:52:05 -0300

    Multi step form raw version

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,23 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { LoginService } from 'src/app/services/login.service';
+import { Component, OnInit } from "@angular/core";
+import { LoginService } from "src/app/services/login.service";
+
+import { STEP_ITEMS } from "../shared/multi-step/config/multi-step-config";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit {
-  currentRoute: string = 'ops1';
+  multiStepConfig: any;
+  formData: any;
+
+  currentRoute: string = "ops1";
   routeConfig: any = {
     routes: [
-      { route: 'Multi Step Form', toRoute: 'ops1' },
-      { route: 'Ops. #2', toRoute: 'ops2' },
-      { route: 'Ops. #3', toRoute: 'ops3' },
+      { route: "Multi Step Form", toRoute: "ops1" },
+      { route: "Ops. #2", toRoute: "ops2" },
+      { route: "Ops. #3", toRoute: "ops3" },
     ],
   };
   constructor(private loginService: LoginService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.multiStepConfig = STEP_ITEMS;
+    this.formData = {};
+  }
+
+  onFormSubmit(formData: any): void {
+    this.formData = formData;
+
+    // post form data here
+    alert(JSON.stringify(this.formData));
+  }
 
   goToRoute(route) {
     this.currentRoute = route;
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DashboardComponent } from './dashboard.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { ContentComponent } from './content/content.component';
-import { MultiStepComponent } from '../shared/multi-step/multi-step.component';
-import { HeaderComponent } from '../shared/header/header.component';
+import { DashboardRoutingModule } from "./dashboard-routing.module";
+import { DashboardComponent } from "./dashboard.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { ContentComponent } from "./content/content.component";
+import { MultiStepComponent } from "../shared/multi-step/multi-step.component";
+import { HeaderComponent } from "../shared/header/header.component";
+
+import { FormatTitlePipe } from "../../pipes/format-title.pipe";
 
 @NgModule({
   declarations: [
@@ -15,7 +18,13 @@ import { HeaderComponent } from '../shared/header/header.component';
     ContentComponent,
     MultiStepComponent,
     HeaderComponent,
+    FormatTitlePipe,
   ],
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class DashboardModule {}
